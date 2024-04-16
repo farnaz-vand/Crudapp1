f3747070a1336ec8e97f30e917a30a928a90817c
farnaz <109223719+farnaz-vand@users.noreply.github.com>
2024-04-16 09:59:33 +0330

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import TodoList from '../features/todos/TodoList';
-import TodoForm from '../features/todos/TodoForm';
-import Navbar from './components/layout/Navbar'; 
+import TodoForm from './../features/todos/TodoForm';
+import TodoList from './../features/todos/TodoList';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="container mx-auto my-8">
-        <h1 className="text-2xl font-bold mb-4">Crud</h1>
-        <TodoForm />
-        <TodoList />
-      </div>
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 };
